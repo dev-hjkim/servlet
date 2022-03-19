d4448201e662a6f442cd3ed471b7a43ecf912af5
dev-hjkim <dev--test@naver.com>
2022-03-19 17:07:00 +0900

    220319, servlet listener & filter

---
+++ src/main/java/com/example/MyListener.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("contextInitialized");
+        sce.getServletContext().setAttribute("name", "keesun");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed");
+    }
+}
