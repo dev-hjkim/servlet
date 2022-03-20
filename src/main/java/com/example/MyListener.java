d8efda2bf0935b42c7e5cf2b611851cf7c992eeb
dev-hjkim <dev--test@naver.com>
2022-03-20 23:05:33 +0900

    220320, way to use IoC container at servlet

--- src/main/java/com/example/MyListener.java
+++ src/main/java/com/example/MyListener.java
@@ -7,7 +7,7 @@ public class MyListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("contextInitialized");
-        sce.getServletContext().setAttribute("name", "keesun");
+        sce.getServletContext().setAttribute("name", "hjkim");
     }
 
     @Override
