47423dc163b4f6537e6017350a8aaff0f07ee4b9
dev-hjkim <dev--test@naver.com>
2022-03-24 00:01:20 +0900

    220324, custom bean

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,9 +12,9 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/{id}")
     @ResponseBody
-    public String hello() {
+    public String hello(@RequestBody String body) {
         return "Hello, " + helloService.getName();
     }
 
